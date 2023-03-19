c8d57753410cb6ae0191aa5405311c048534b845
TPhuoc <tranphuoc17012001.gmail>
2023-03-19 15:36:26 +0700

    do an java

---
+++ src/main/java/com/hotel/hotelmanagement/AddRoomController.java
@@ -0,0 +1,67 @@
+package com.hotel.hotelmanagement;
+
+import javafx.fxml.FXML;
+
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.stage.StageStyle;
+
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+import static com.hotel.hotelmanagement.RoomController.roomList;
+import static com.hotel.hotelmanagement.RoomController.rooms;
+
+public class AddRoomController implements Initializable {
+
+    @FXML
+    private Button add;
+
+    @FXML
+    private TextField number;
+
+    @FXML
+    private TextField price;
+
+    @FXML
+    private TextField type;
+
+    private Connection connection;
+
+    private DBConnection dbConnection;
+
+    private PreparedStatement pst;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        dbConnection = new DBConnection();
+        connection = dbConnection.getConnection();
+    }
+
+    public void handleAddAction(javafx.event.ActionEvent actionEvent) {
+        String query = "INSERT INTO rooms (roomNumber, roomType, price) VALUES (?,?,?)";
+        try {
+            pst = connection.prepareStatement(query);
+            pst.setString(1, number.getText());
+            pst.setString(2, type.getText());
+            pst.setString(3, price.getText());
+            roomList.add(new Room(Integer.parseInt(number.getText()), Integer.parseInt(price.getText()), type.getText(), "Not Booked"));
+            rooms.add(new Room(Integer.parseInt(number.getText()), Integer.parseInt(price.getText()), type.getText(), "Not Booked"));
+            pst.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        OptionPane("Create Room Successful", "Message");
+    }
+    private void OptionPane(String message, String title) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.initStyle(StageStyle.UTILITY);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+}
